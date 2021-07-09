e57fe1de0e248492687f6b316d858778269d1ea3
joaomdinis <joaomdinis2@gmail.com>
2021-07-09 17:19:37 -0300

    [update] psw

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 public class Main{
     public static void main(String[] args){
+        //Scanner cc = new Scanner(System.in);
+
+        /*System.out.println("==== Crie sua conta no Facebook ====");
+        System.out.println("Digite o url:");
+        Sy0stem.out.println("Digite o usuario:");
+        System.out.println("Digite a senha:");
+*/
         ContaFacebook cnt = new ContaFacebook("teste", "toto", "12");
         System.out.println(cnt.toString());
-        cnt.setPsw();
+
+        cnt.setPsw(cnt.getPsw(), "novaSenha");
+        System.out.println(cnt.toString());
+        
     }
 }
